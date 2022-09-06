ddc1d2c19284bbab08a028999c540658f1c0a99c
muhammadalkurdy <jobirtolaganov59@gmail.com>
2022-09-06 17:11:41 +0500

    javascript

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+alert ("Welcome!");
+let ism = prompt ("ismingizni ayting");
+let yosh =  prompt ("Yoshingizni ayting");
+let manzil = prompt ("Yashash joyingizni ayting");
+alert("bzni saytimizga hush kelipsz");
+console.log("mening ismim " + ism + " mening yoshim " + yosh + " nan" + "." + " ho " + manzil + " hello, world.")
+
+btn.addEventListener('click', () => {
+    let yil = prompt("Enter your birth year");
+    let sana = 2022 - yil;
+    console.log("sening " + sana + " yoshim.");
+})
